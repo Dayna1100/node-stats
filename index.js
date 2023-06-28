3e733628efd57dafac03cbc1878bb8a4b34c6a36
Dayna Tynan <Dayna_Regan@yahoo.com>
2023-06-28 11:19:08 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// The smallest number in the array (use the min() function)
+// The largest number in the array (use the max() function)
+// The sum of the numbers in the array (use the sumSimple() function)
+// The average (or mean) value in the array (use the mean() function)
+
+
+let ss = require("simple-statistics");
+const { min } = require("@aeth/min.js");
+
+let numbers =[62, 60, 53, 9, 11, 14, 15, 16, 17, 65, 88];
+console.log(min([numbers]));
+
+
+// let message = "Hello World";
+// console.log(message);
